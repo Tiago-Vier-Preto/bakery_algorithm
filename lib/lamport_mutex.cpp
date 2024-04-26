b2fcd4cb8dbac4c2d63f04761f31d91245e8f51b
Tiago-Vier-Preto <tvpreto@gmail.com>
2024-04-26 17:05:49 -0300

    Refactor Makefile and add new tests for Lamport mutex algorithm

--- lib/lamport_mutex.cpp
+++ lib/lamport_mutex.cpp
@@ -1,29 +1,38 @@
-#include <iostream>
-#include <vector>   
+#include <iostream>   
 #include "../include/lamport_lib.hpp"
 
 #define MAX_THREADS 100
 
-static std::vector<bool> choosing(1, false);
-static std::vector<int> ticket(1, 0);
+static int choosing[MAX_THREADS];
+static int ticket[MAX_THREADS];
 
 void lamport_mutex_init() {
-    choosing.resize(MAX_THREADS, false); 
-    ticket.resize(MAX_THREADS, 0); 
+    for (int i = 0; i < MAX_THREADS; i++) {
+        choosing[i] = false;
+        ticket[i] = 0;
+    }
 }
 
 void lamport_mutex_lock (int thread_id) {
-    set_choosing(thread_id - 1, true);
-    set_ticket(thread_id - 1, get_max_ticket() + 1);
-    set_choosing(thread_id - 1, false);
+    int i = thread_id - 1;
+    set_choosing(i, true);
+    set_ticket(i, get_max_ticket() + 1);
+    set_choosing(i, false);
     for (int j = 0; j < MAX_THREADS; j++) {
         while (get_choosing(j)) {} // do nothing
-        while ((get_ticket(j) != 0) && ((get_ticket(j) < get_ticket(thread_id - 1)) || ((get_ticket(j) == get_ticket(thread_id - 1)) && (j < thread_id - 1)))) {} // do nothing
+        while ((get_ticket(j) != 0) && ((get_ticket(j) < get_ticket(i)) || ((get_ticket(j) == get_ticket(i)) && (j < i)))) {} // do nothing
     }
 }
 
 void lamport_mutex_unlock (int thread_id){
     set_ticket(thread_id - 1, 0);
+}
+
+static int get_max_ticket() {
+    int max = ticket[0];
+    for (int i = 1; i < MAX_THREADS; i++) 
+        max = ticket[i] > max ? ticket[i] : max;
+    return max;
 }
 
 // Getters and setters
@@ -41,11 +50,4 @@ static void set_ticket(int index, int value) {
 
 static int get_ticket(int index) {
     return ticket[index];
-}
-
-static int get_max_ticket() {
-    int max = ticket[0];
-    for (int i = 1; i < MAX_THREADS; i++) 
-        max = ticket[i] > max ? ticket[i] : max;
-    return max;
 }
